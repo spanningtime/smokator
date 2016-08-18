2b1d0249575371ba0134411801aa89f226ca2b4b
William Knight <kniwilliam@gmail.com>
2016-08-18 14:10:23 -0700

    Close #3 Create givers migration

---
+++ migrations/20160818140310_givers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+exports.up = function(knex) {
+  return knex.schema.createTable('givers', (table) => {
+    table.increments();
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .index();
+    table.string('place_id')
+      .notNullable()
+      .unique()
+    table.dateTime('expiration')
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('givers');
+};
