b883ecff3eb0e0b9e972d287b5e22d51d0f4c0b3
Jesse Seligman <jesseseligman@gmail.com>
2016-08-18 13:53:53 -0700

    Add knex script and create users migration close #2

---
+++ migrations/20160818134525_users.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('phone').unique().notNullable().defaultTo('');
+    table.integer('avatar_id').notNullable().defaultTo(0);
+    table.string('email').unique().notNullable().defaultTo('');
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.string('first_name').notNullable().defaultTo('');
+    table.string('last_name').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
