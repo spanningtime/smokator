67addf0501c603eeea6dca9963da1989ecea4872
William Knight <kniwilliam@gmail.com>
2016-08-18 18:09:36 -0700

    Close #5 Create chat_messages migration

---
+++ migrations/20160818174431_chat_messages.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('chat_messages', (table) => {
+    table.increments();
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE');
+    table.integer('chat_id')
+      .notNullable()
+      .references('id')
+      .inTable('chats')
+      .onDelete('CASCADE');
+    table.text('message_text')
+      .notNullable()
+    table.dateTime('expiration');
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('chat_messages')
+};
