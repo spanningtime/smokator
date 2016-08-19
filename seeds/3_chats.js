f3acb08f08036006083f818f40b9281bdc52ff40
William Knight <kniwilliam@gmail.com>
2016-08-18 18:20:31 -0700

    Close #8 Create chats seed

---
+++ seeds/3_chats.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.seed = function(knex) {
+  return knex('chats').del()
+    .then(() => {
+      return knex('chats').insert([{
+        id: 1,
+        bummer_id: 1,
+        giver_id: 2,
+        expiration: new Date('2016-08-15 15:36:16 UTC')
+        created_at: new Date('2016-08-15 14:36:16 UTC'),
+        updated_at: new Date('2016-08-15 14:36:16 UTC')
+      }]);
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('chats_id_seq', (SELECT MAX(id) FROM chats));"
+      );
+    });
+};
