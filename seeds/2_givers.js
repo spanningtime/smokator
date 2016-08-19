a91a6260f806195e4595fb27d9de068b3054e635
Jesse Seligman <jesseseligman@gmail.com>
2016-08-18 18:42:46 -0700

    Fix givers seed

--- seeds/2_givers.js
+++ seeds/2_givers.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
         id: 1,
         user_id: 2,
         place_id: 'ChIJG2K5JrtqkFQRsT22hSRqrrc',
-        expiration: new Date('2016-08-15 17:26:16 UTC')
+        expiration: new Date('2016-08-15 17:26:16 UTC'),
         created_at: new Date('2016-08-15 14:26:16 UTC'),
         updated_at: new Date('2016-08-15 14:26:16 UTC')
       }]);
