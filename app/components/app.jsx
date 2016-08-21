e1b725492f5f91c71f9edd7992d89e19495ee91f
Jesse Seligman <jesseseligman@gmail.com>
2016-08-20 17:50:22 -0700

    Start login mockup

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,9 +1,82 @@
-import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import Paper from 'material-ui/Paper';
 import React from 'react';
 
 const App = React.createClass({
+  getInitialState() {
+    return {
+      isLoggedIn: true,
+    }
+  },
+
   render() {
-    return <RaisedButton label="Hello world" />;
+
+    const styleMenu = {
+      iconStyle: {
+        width: 48,
+        height: 48,
+        paddingBottom: 0,
+        display: this.state.isLoggedIn ? 'inline-block' : 'none'
+      },
+      style: {
+        width: 64,
+        height: 64,
+        padding: 0
+      }
+    };
+
+    const stylePaper = {
+      backgroundColor: '#e5f3e9'
+    }
+
+    const styleContainer = {
+      display: 'flex',
+      flexDirection: 'row',
+      alignContent: 'space-around',
+      alignItems: 'center',
+      height: '100px',
+      paddingTop: '15px',
+      marginLeft: '40px',
+      justifyContent: 'space-between'
+    }
+
+    const styleTitle = {
+      fontSize: '2em',
+      color: '#ff7f66'
+    }
+
+    return <Paper style={stylePaper}
+      >
+      <div style={styleContainer}>
+        <div style={styleTitle}>
+          smokator
+        </div>
+        <IconButton
+          iconStyle={styleMenu.iconStyle}
+          style={styleMenu.style}
+        >
+          <NavigationMenu color={'#ff7f66'}/>
+        </IconButton>
+      </div>
+
+
+    </Paper>;
+    {/* <AppBar
+    iconElementRight={
+      <IconButton
+        iconStyle={styleMenu.iconStyle}
+        style={styleMenu.style}
+      >
+        <NavigationMenu color={'#ff7f66'}/>
+      </IconButton>}
+    showMenuIconButton={false}
+    title="smokator"
+    titleStyle={styleTitle}
+     >
+
+    </AppBar>; */}
   }
 });
 
--- app/index.jsx
+++ app/index.jsx
@@ -2,12 +2,19 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from 'components/Routes';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#e5f3e9'
+  }
+});
+
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <Routes />
   </MuiThemeProvider>,
   document.getElementById('app')
