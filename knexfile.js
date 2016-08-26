cb7d9706e6f06f6cf0d343ae918cedc6028845b0
Jesse Seligman <jesseseligman@gmail.com>
2016-08-25 18:42:30 -0700

    Lint knexfile.js close #118

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   development: {
-   client: 'pg',
-   connection: 'postgres://localhost/smokator_dev'
- },
+    client: 'pg',
+    connection: 'postgres://localhost/smokator_dev'
+  },
 
   production: {
-   client: 'pg',
-   connection: process.env.DATABASE_URL
- }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
