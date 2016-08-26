ada3111040fed8d75297f51633411a75756f83ae
Jesse Seligman <jesseseligman@gmail.com>
2016-08-25 17:23:21 -0700

    Lint chats.js route close #106

--- routes/chats.js
+++ routes/chats.js
@@ -7,50 +7,51 @@ const ev = require('express-validation');
 const chatValidations = require('../validations/chats');
 const chatMessageValidations = require('../validations/chatMessages');
 const { camelizeKeys, decamelizeKeys } = require('humps');
-const jwt = require('jsonwebtoken');
 const { checkAuth } = require('../middleware');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.post('/api/chats', checkAuth, ev(chatValidations.post), (req, res, next) => {
-  const { bummerId, giverId } = req.body;
-  const expiration = new Date(Date.now() + 1000 * 60 * 60);
-  const row = decamelizeKeys({ bummerId, giverId, expiration });
-  knex('chats')
-    .insert(row, '*')
-    .then((rows) => {
-      const chat = camelizeKeys(rows[0]);
+// eslint-disable-next-line max-len
+router.post('/api/chats', checkAuth, ev(chatValidations.post),
+  (req, res, next) => {
+    const { bummerId, giverId } = req.body;
+    const expiration = new Date(Date.now() + 1000 * 60 * 60);
+    const row = decamelizeKeys({ bummerId, giverId, expiration });
 
-      res.send(chat);
-    })
-    .catch((err) => {
-      next(boom.wrap(err));
-    });
-});
+    knex('chats')
+      .insert(row, '*')
+      .then((rows) => {
+        const chat = camelizeKeys(rows[0]);
+
+        res.send(chat);
+      })
+      .catch((err) => {
+        next(boom.wrap(err));
+      });
+  });
 
 router.post('/api/chats/:chatId', checkAuth, ev(chatMessageValidations.post),
   (req, res, next) => {
+    const chatId = Number.parseInt(req.params.chatId);
+    const { userId, messageText } = req.body;
+    const expiration = new Date(Date.now() + 1000 * 60 * 60);
+    const row = decamelizeKeys({ userId, chatId, messageText, expiration });
 
-  const chatId = Number.parseInt(req.params.chatId);
-  const { userId, messageText } = req.body;
-  const expiration = new Date(Date.now() + 1000 * 60 * 60);
-  const row = decamelizeKeys({ userId, chatId, messageText, expiration });
+    knex('chat_messages')
+      .insert(row, '*')
+      .then((rows) => {
+        const chatMessage = camelizeKeys(rows[0]);
 
-  knex('chat_messages')
-    .insert(row, '*')
-    .then((rows) => {
-      const chatMessage = camelizeKeys(rows[0]);
-
-      res.send(chatMessage);
-    })
-    .catch((err) => {
-      next(boom.wrap(err));
-    });
-});
+        res.send(chatMessage);
+      })
+      .catch((err) => {
+        next(boom.wrap(err));
+      });
+  });
 
 router.get('/api/chats/:chatId', (req, res, next) => {
   const chatId = Number.parseInt(req.params.chatId);
-  console.log(chatId)
+
   knex('chat_messages')
     .where('chat_id', chatId)
     .then((rows) => {
