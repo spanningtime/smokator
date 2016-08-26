d7c36e2c6e2dbeec7fd9ae9e4ae1a7e8097635c8
Jesse Seligman <jesseseligman@gmail.com>
2016-08-25 17:46:28 -0700

    Lint places.js route close #108

--- routes/places.js
+++ routes/places.js
@@ -2,35 +2,33 @@
 
 const boom = require('boom');
 const express = require('express');
-const knex = require('../knex');
-const { camelizeKeys, decamelizeKeys } = require('humps');
-const jwt = require('jsonwebtoken');
 const request = require('request-promise');
 
-const router = express.Router(); //eslint-disable-line new-cap
+const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/api/places/:location', (req, res, next) => {
-  const { location } = req.params
+  const { location } = req.params;
 
-  request({ uri:
+  request({ uri: // eslint-disable-next-line max-len
     `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=150&type=bar&key=AIzaSyAFgyrpeZwrVMRD9zu5o8qNFOVH6FlO1Ps`,
     json: true })
       .then((response) => {
-        if (response.results.length === 0) {
+        if (response.results.length === 0) { // eslint-disable-next-line max-len
           throw boom.create(400, 'There are no bars in your area. Try again later.');
         }
 
         const bars = response.results.map((bar) => {
           const name = bar.name;
           const placeId = bar.place_id;
+
           return { name, placeId };
         });
 
-        return res.send(bars)
+        return res.send(bars);
       })
       .catch((err) => {
         next(boom.wrap(err));
       });
-})
+});
 
 module.exports = router;
