ec7da78b43ccaeb27761a3ea87c7270ce4ca641d
William Knight <kniwilliam@gmail.com>
2016-08-25 23:36:15 -0700

    Lint Routes.jsx

--- app/components/Routes.jsx
+++ app/components/Routes.jsx
@@ -1,35 +1,37 @@
-import { Route, Router, browserHistory, IndexRoute } from 'react-router';
-import App from 'components/App';
-import React from 'react';
-import LoginForm from 'components/LoginForm';
-import LoginButtons from 'components/LoginButtons';
-import StatusButtons from 'components/StatusButtons';
-import Home from 'components/Home';
-import Givers from 'components/Givers';
-import Profile from 'components/Profile';
-import Register from 'components/Register';
-import ChatWindow from 'components/ChatWindow';
-import GiversWaiting from 'components/GiversWaiting';
+
+/* eslint-disable sort-imports */
+import { browserHistory, IndexRoute, Router, Route } from 'react-router';
 import About from 'components/About';
+import App from 'components/App';
+import ChatWindow from 'components/ChatWindow';
+import Givers from 'components/Givers';
+import GiversWaiting from 'components/GiversWaiting';
+import Home from 'components/Home';
+import LoginButtons from 'components/LoginButtons';
+import LoginForm from 'components/LoginForm';
+import Profile from 'components/Profile';
+import React from 'react';
+import Register from 'components/Register';
+import StatusButtons from 'components/StatusButtons';
 
-
+/* eslint-enable sort-imports */
 
 const Routes = React.createClass({
   render() {
     return <Router history={browserHistory}>
       <Route component={App} path="/">
-        <IndexRoute component={LoginButtons}/>
-        <Route component={LoginForm} path="login" />
-        <Route component={StatusButtons} path="status"/>
-        <Route component={GiversWaiting} path="waiting" />
-        <Route component={Home} path="home" />
-        <Route component={Givers} path="givers" />
-        <Route component={Profile} path="profile/:userId" />
-        <Route component={Register} path="register" />
-        <Route component={ChatWindow} path="chats/:chatId" />
+        <IndexRoute component={LoginButtons} />
         <Route component={About} path="about" />
+        <Route component={ChatWindow} path="chats/:chatId" />
+        <Route component={GiversWaiting} path="waiting" />
+        <Route component={Givers} path="givers" />
+        <Route component={Home} path="home" />
+        <Route component={LoginForm} path="login" />
+        <Route component={Profile} path="profile/:userId" />
+        <Route component={StatusButtons} path="status" />
+        <Route component={Register} path="register" />
       </Route>
-    </Router>
+    </Router>;
   }
 });
 
