cf24ced215f73103de7acaeae2d64f233dbce3b4
Jesse Seligman <jesseseligman@gmail.com>
2016-08-25 17:38:51 -0700

    Lint givers.js route close #107

--- routes/givers.js
+++ routes/givers.js
@@ -1,20 +1,19 @@
 'use strict';
 
-const bcrypt = require('bcrypt-as-promised');
 const boom = require('boom');
 const express = require('express');
 const knex = require('../knex');
 const ev = require('express-validation');
 const validations = require('../validations/givers');
 const { camelizeKeys, decamelizeKeys } = require('humps');
-const jwt = require('jsonwebtoken');
 const { checkAuth } = require('../middleware.js');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/api/givers/:placeId', (req, res, next) => {
-  const placeId = (req.params.placeId);
+  const placeId = req.params.placeId;
 
+// eslint-disable-next-line max-len
   knex.select('first_name', 'about_me', 'avatar_id', 'phone', 'user_id', 'place_id', 'expiration')
     .from('users')
     .innerJoin('givers', 'users.id', 'givers.user_id')
@@ -31,29 +30,30 @@ router.get('/api/givers/:placeId', (req, res, next) => {
     });
 });
 
-router.post('/api/givers', checkAuth, ev(validations.post), (req, res, next) => {
-  const { userId, placeId } = req.body;
-  const expiration = new Date(Date.now() + 1000 * 60 * 60 * 3 );
-  const row = decamelizeKeys({ userId, placeId, expiration });
+router.post('/api/givers', checkAuth, ev(validations.post),
+  (req, res, next) => {
+    const { userId, placeId } = req.body;
+    const expiration = new Date(Date.now() + 1000 * 60 * 60 * 3);
+    const row = decamelizeKeys({ userId, placeId, expiration });
 
-  knex('givers')
-    .where('user_id', userId)
-    .del()
-    .then(() => {
-      return knex('givers')
-        .insert(row, '*')
-        .then((rows) => {
-          const giver = camelizeKeys(rows[0]);
+    knex('givers')
+      .where('user_id', userId)
+      .del()
+      .then(() => { // eslint-disable-line arrow-body-style
+        return knex('givers')
+          .insert(row, '*') // eslint-disable-line semi
+      })
+      .then((rows) => {
+        const giver = camelizeKeys(rows[0]);
 
-          return giver;
-        })
-    })
-    .then((giver) => {
-      res.send(giver);
-    })
-    .catch((err) => {
-      next(boom.wrap(err));
-    });
-});
+        return giver;
+      })
+      .then((giver) => {
+        res.send(giver);
+      })
+      .catch((err) => {
+        next(boom.wrap(err));
+      });
+  });
 
 module.exports = router;
