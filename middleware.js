d363aacf3f708bee472925b01a125d2bf259f86c
Jesse Seligman <jesseseligman@gmail.com>
2016-08-25 18:41:11 -0700

    Lint middleware.js close #119

--- middleware.js
+++ middleware.js
@@ -3,14 +3,15 @@
 const jwt = require('jsonwebtoken');
 
 const checkAuth = function(req, res, next) {
-  jwt.verify(req.cookies.accessToken, process.env.SESSION_SECRET, (err, decoded) => {
-    if (err) {
-      return res.sendStatus(401);
-    }
+  jwt.verify(req.cookies.accessToken, process.env.SESSION_SECRET,
+    (err, decoded) => {
+      if (err) {
+        return res.sendStatus(401);
+      }
 
-    req.token = decoded;
-    next();
-  });
-}
+      req.token = decoded;
+      next();
+    });
+};
 
 module.exports = { checkAuth };
