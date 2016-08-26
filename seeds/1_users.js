fe76bd9ca05d63beb1f41d44bc2837a7cba3334d
Jesse Seligman <jesseseligman@gmail.com>
2016-08-25 19:03:41 -0700

    Disable linter for seeds

--- seeds/1_users.js
+++ seeds/1_users.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* eslint-disable */
+
 exports.seed = function(knex) {
   return knex('users').del()
     .then(() => {
--- seeds/4_chat_messages.js
+++ seeds/4_chat_messages.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* eslint-disable */
+
 exports.seed = function(knex) {
   return knex('chat_messages').del()
     .then(() => {
